1c40aaac7fbd712330adda23cb357748989acf63
aaronbrezel <35546183+aaronbrezel@users.noreply.github.com>
2018-02-01 14:01:53 -0600

    Revert "Test with Alex"
    This reverts commit cdf6dc444e40249f7d12f76874e0400150b2ca28.

--- ClassExample.R
+++ ClassExample.R
@@ -10,8 +10,11 @@ print("A third line")
 
 print("Hello Alex")
 <<<<<<< HEAD
+<<<<<<< HEAD
 print("It works")
 
 print("fuck off Aaron")
+=======
+>>>>>>> parent of cdf6dc4... Test with Alex
 =======
 >>>>>>> parent of cdf6dc4... Test with Alex
