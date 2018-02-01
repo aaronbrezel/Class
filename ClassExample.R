663ab880849b7144e219e85135eadb662a12a836
aaronbrezel <35546183+aaronbrezel@users.noreply.github.com>
2018-02-01 14:08:21 -0600

    Revert "Github practice"
    This reverts commit f9e4483b4b7bc7a33f3d6f1be147bf7c2e9db41d.

--- ClassExample.R
+++ ClassExample.R
@@ -8,23 +8,10 @@ print("A new line")
 
 print("A third line")
 
+
 print("Hello Alex")
-<<<<<<< HEAD
+
 print("It works")
 
 print("nice change! Aaron")
 
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("It works")
-
-print("fuck off Aaron")
-=======
->>>>>>> parent of cdf6dc4... Test with Alex
-=======
->>>>>>> parent of cdf6dc4... Test with Alex
-=======
->>>>>>> parent of cdf6dc4... Test with Alex
->>>>>>> 00e7a5412078cc1e47667e8dda60f29a69e13890
